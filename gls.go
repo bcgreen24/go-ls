d6378f73d148d13ffce6a1df5aa15309ce821af7
Bryan Green <bgreen4@ucmerced.edu>
2014-07-16 23:29:39 -0500

    Added option to view modification date/time and shortened flag for file size

--- gls.go
+++ gls.go
@@ -1,7 +1,6 @@
 package main
 
 import "go-colortext"
-
 import (
 	"flag"
 	"fmt"
@@ -11,13 +10,15 @@ import (
 )
 
 func main() {
-	var option = flag.Bool("filesize", false, "Show file sizes")
+	var option = flag.Bool("size", false, "Show file sizes")
+	var mod_date = flag.Bool("date", false, "Show modification dates")
 	flag.Parse()
 	dir, _ := os.Getwd()
 	file_list, _ := filepath.Glob(dir + "/*")
 	for _, file := range file_list {
 		f, _ := os.Open(file)
 		fi, _ := f.Stat()
+		
 		switch mode := fi.Mode(); {
 		case mode.IsDir():
 			ct.ChangeColor(ct.Green, true, ct.None, false)
@@ -26,6 +27,10 @@ func main() {
 			ct.ChangeColor(ct.White, true, ct.None, false)
 			if *option == true {
 				fmt.Printf(strconv.FormatInt(fi.Size(), 10) + " ")
+			}
+			if *mod_date == true {
+				const layout = "Jan 2, 2006 at 3:04pm"
+				fmt.Printf(fi.ModTime().Format(layout) + " ")
 			}
 			fmt.Printf(file + "\n")
 		}
