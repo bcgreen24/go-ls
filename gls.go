a54cec73a9f772f5390a4204dfcfb09d103cff11
Bryan Green <bgreen4@ucmerced.edu>
2014-07-21 22:26:53 -0500

    modified code to use maps

--- gls.go
+++ gls.go
@@ -2,7 +2,6 @@ package main
 
 import "go-colortext"
 import (
-	"container/list"
 	"flag"
 	"fmt"
 	"os"
@@ -10,57 +9,51 @@ import (
 	"strconv"
 )
 
+const layout = "Jan 2, 2006 at 3:04pm"
+
 func main() {
 	var option = flag.Bool("size", false, "Show file sizes")
 	var mod_date = flag.Bool("date", false, "Show modification dates")
+
 	flag.Parse()
 	dir, _ := os.Getwd()
 	file_list, _ := filepath.Glob(dir + "/*")
-	//var regularFiles = list.New()
-	//var dirFiles = list.New()
 
-	var regularFiles = map[string]map[string]string{}
-	var dirFiles = map[string]map[string]string{}
+	var regularFiles = make(map[string]map[string]string)
+	var dirFiles = make(map[string]map[string]string)
 
 	for _, file := range file_list {
 		f, _ := os.Open(file)
 		fi, _ := f.Stat()
 		switch mode := fi.Mode(); {
 		case mode.IsDir():
-			dirFiles["name"] = map[string]string{
-				"size":strconv.FormatInt(fi.Size(), 10),
-				"data":
+			dirFiles[fi.Name()] = map[string]string{
+				"size": strconv.FormatInt(fi.Size(), 10),
+				"date": fi.ModTime().Format(layout),
 			}
 		case mode.IsRegular():
-			regularFiles.PushFront(fi.Name())
+			regularFiles[fi.Name()] = map[string]string{
+				"size": strconv.FormatInt(fi.Size(), 10),
+				"date": fi.ModTime().Format(layout),
+			}
 		}
 	}
-	for x := dirFiles.Front(); x != nil; x = x.Next() {
-		fmt.Println(x.Value)
+
+	for key, _ := range dirFiles {
+		ct.ChangeColor(ct.Green, true, ct.None, false)
+		fmt.Println(key)
 	}
 
-	for x := regularFiles.Front(); x != nil; x = x.Next() {
-		fmt.Println(x.Value)
-	}
-
-	for _, file := range file_list {
-		f, _ := os.Open(file)
-		fi, _ := f.Stat()
-		switch mode := fi.Mode(); {
-		case mode.IsDir():
-			ct.ChangeColor(ct.Green, true, ct.None, false)
-			fmt.Printf(filepath.Base(file) + "\n")
-		case mode.IsRegular():
-			ct.ChangeColor(ct.White, true, ct.None, false)
-			if *option == true {
-				fmt.Printf(strconv.FormatInt(fi.Size(), 10) + " | ")
-			}
-			if *mod_date == true {
-				const layout = "Jan 2, 2006 at 3:04pm"
-				fmt.Printf(fi.ModTime().Format(layout) + " | ")
-			}
-			fmt.Printf(filepath.Base(file) + "\n")
+	for key, value := range regularFiles {
+		ct.ChangeColor(ct.White, true, ct.None, false)
+		line := key
+		if *option == true {
+			line += " | " + value["size"]
 		}
+		if *mod_date == true {
+			line += " | " + value["date"]
+		}
+		fmt.Println(line)
 	}
 	ct.ResetColor()
 }
